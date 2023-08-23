9c2d6cbdc91f1011b3843012b26c0c80d0f27246
mike <drmikegardner@gmail.com>
2023-08-23 14:55:41 +0100

    plan search: remove levels

--- assets/plan.js
+++ assets/plan.js
@@ -8,11 +8,8 @@ $(document).ready( function () {
         "language": { "info": "Showing _START_ to _END_ of _TOTAL_ programmes",
                       "infoFiltered":   "(filtered from _MAX_)",
                        "search": "Filter:" },
-        "columnDefs": [ { "targets": 5, "orderable": false } ]
+        "columnDefs": [ { "targets": 4, "orderable": false } ]
       });
 
-      // add buttons html to .levels
-      var levelCode = '<label for="levelfilters" class="label">Level:</label><div class="btn-group btn-group-toggle" id="levelfilters" data-toggle="buttons"><label class="btn btn-outline-primary active" for="ALL"><input type="radio" name="level" id="ALL" value="ALL" autocomplete="off" checked=""> All</label><label class="btn btn-outline-primary" for="UG"><input type="radio" name="year" id="UG" value="1" autocomplete="off">Undergraduate</label><label class="btn btn-outline-primary" for="UG"><input type="radio" name="year" id="PG" value="1" autocomplete="off">Postgraduate</label></div>';
-      $('.levels').append(levelCode);
 
 } );
