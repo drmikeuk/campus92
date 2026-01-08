e1270cb090b243d528d7974ce531752113823772
mike <drmikegardner@gmail.com>
2026-01-08 18:35:14 +0000

    tweak highlight so can change keyword

--- assets/automationHighlight.js
+++ assets/automationHighlight.js
@@ -1,9 +1,28 @@
 // highlight text using https://markjs.io/
 
-var searchTerm = "pharmacy";
-
-var options = {"className": "match"}
-
 var context = document.querySelector(".content");
-var instance = new Mark(context);
-instance.mark(searchTerm, options);
+var markInstance = new Mark(context);
+
+$(document).ready( function () {
+  // Highlight on load (as input is prepopulated)
+  highlight();
+
+  // Highlight on change
+  $("input[name='highlight']").on("input", highlight);
+
+} );
+
+
+// Function to mark highlighted text
+function highlight(){
+  var options = {"className": "match"};
+  var keyword = $("input[name='highlight']").val();
+  //console.log ("Highlight: " + keyword);
+  // UNMARK (remove previous marked elements) and MARK the new keyword
+  markInstance.unmark({
+  	done: function(){
+    	markInstance.mark(keyword, options);
+    }
+  });  
+}
+
